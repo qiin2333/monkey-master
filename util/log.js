eb19256edf352579fb394f1ca978ef90e9bebcc0
qiin <chouq@gmail.com>
2024-06-12 16:33:50 +0800

    sync: update log plugin

--- util/log.js
+++ util/log.js
@@ -1,15 +1,15 @@
-import * as log from 'https://deno.land/std/log/mod.ts';
+import * as log from 'https://deno.land/std@0.224.0/log/mod.ts';
 import { format as dateFormat } from 'https://deno.land/std@0.89.0/datetime/mod.ts';
 
-await log.setup({
+log.setup({
     handlers: {
-        console: new log.handlers.ConsoleHandler('DEBUG', {
+        console: new log.ConsoleHandler('DEBUG', {
             formatter: (logRecord) =>
                 `[${dateFormat(new Date(), 'yyyy-MM-dd HH:mm:ss.SSS')}] ${
                     logRecord.msg
                 }`,
         }),
-        file: new log.handlers.FileHandler('WARNING', {
+        file: new log.FileHandler('WARN', {
             filename: `${Deno.cwd()}/run.log`,
             formatter: '{levelName} {datetime} {msg}',
         }),
